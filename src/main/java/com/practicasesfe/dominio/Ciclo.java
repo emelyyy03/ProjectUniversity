2e1b7f9bbbd81910d9d6b54f7cf4aec5389609aa
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-06-10 09:20:00 -0600

    Se creo las propiedades y se encapsularon en la clase Ciclo de dominio

--- src/main/java/com/practicasesfe/dominio/Ciclo.java
+++ src/main/java/com/practicasesfe/dominio/Ciclo.java
@@ -1,4 +1,61 @@
 package com.practicasesfe.dominio;
 
+import java.sql.Date;
+
 public class Ciclo {
+
+    private int idCiclo;
+    private String nombreCiclo;
+    private Date fechaInicio;
+    private Date fechaFin;
+
+    // Constructor vacío
+    public Ciclo() {
+    }
+
+    // Constructor completo
+    public Ciclo(int idCiclo, String nombreCiclo, Date fechaInicio, Date fechaFin) {
+        this.idCiclo = idCiclo;
+        this.nombreCiclo = nombreCiclo;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+
+    // Getters y Setters
+    public int getIdCiclo() {
+        return idCiclo;
+    }
+
+    public void setIdCiclo(int idCiclo) {
+        this.idCiclo = idCiclo;
+    }
+
+    public String getNombreCiclo() {
+        return nombreCiclo;
+    }
+
+    public void setNombreCiclo(String nombreCiclo) {
+        this.nombreCiclo = nombreCiclo;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    @Override
+    public String toString() {
+        return nombreCiclo;
+    }
 }
