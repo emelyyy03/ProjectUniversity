669356b81ed3e887afee4b3d629772d8ef5446d2
Karla Vanesa Turcios Reyes <tr50001@esfe.agape.edu.sv>
2025-06-11 11:35:09 -0600

    se creo funcionalidad de la clase Aula

--- src/main/java/com/practicasesfe/dominio/Aulas.java
+++ src/main/java/com/practicasesfe/dominio/Aulas.java
@@ -1,4 +1,51 @@
 package com.practicasesfe.dominio;
 
 public class Aulas {
+    private int id;
+    private String nombre;
+    private int capacidad;
+
+    public Aulas() {
+    }
+
+    public Aulas(int id, String nombre, int capacidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    // Método toString
+    @Override
+    public String toString() {
+        return "Aula {" +
+                "ID=" + id +
+                ", Nombre='" + nombre + '\'' +
+                ", Capacidad=" + capacidad +
+                '}';
+    }
 }
