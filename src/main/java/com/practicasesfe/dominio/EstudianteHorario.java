761be7e9316130e3bfe628cee1e82cf55c236680
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-06-12 00:02:02 -0600

    Se realizo la funcionalidad en la clase EstudianteHorario

--- src/main/java/com/practicasesfe/dominio/EstudianteHorario.java
+++ src/main/java/com/practicasesfe/dominio/EstudianteHorario.java
@@ -2,4 +2,42 @@ package com.practicasesfe.dominio;
 
 public class EstudianteHorario {
 
+
+    private int idEstudiante;
+    private int idHorario;
+
+    // Constructor vacío
+    public EstudianteHorario() {
+    }
+
+    // Constructor completo
+    public EstudianteHorario(int idEstudiante, int idHorario) {
+        this.idEstudiante = idEstudiante;
+        this.idHorario = idHorario;
+    }
+
+    // Getters y Setters
+    public int getIdEstudiante() {
+        return idEstudiante;
+    }
+
+    public void setIdEstudiante(int idEstudiante) {
+        this.idEstudiante = idEstudiante;
+    }
+
+    public int getIdHorario() {
+        return idHorario;
+    }
+
+    public void setIdHorario(int idHorario) {
+        this.idHorario = idHorario;
+    }
+
+    @Override
+    public String toString() {
+        return "EstudianteHorario{" +
+                "idEstudiante=" + idEstudiante +
+                ", idHorario=" + idHorario +
+                '}';
+    }
 }
