a632d11633dbf8d34423f50bf21db7fed96e2019
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-06-15 14:56:04 -0600

    Se creo el diseño del formulario de EstudanteHorario

--- src/main/java/com/practicasesfe/Formularios/EstudianteHorario.java
+++ src/main/java/com/practicasesfe/Formularios/EstudianteHorario.java
@@ -4,6 +4,12 @@ import javax.swing.*;
 
 public class EstudianteHorario extends JPanel {
     private JPanel rootPanel;
+    private JTable table1;
+    private JComboBox comboBox1;
+    private JButton asignarHorarioButton;
+    private JTable table2;
+    private JButton eliminarAsignacionButton;
+    private JButton volverButton;
 
     public JPanel getRootPanel() {
         return rootPanel;
