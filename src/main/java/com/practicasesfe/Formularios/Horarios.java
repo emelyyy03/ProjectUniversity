d94db2315e7b0969da5844565a945bd710e90fbf
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-06-15 12:23:33 -0600

    Se esta realizando el formulario de horaios

--- src/main/java/com/practicasesfe/Formularios/Horarios.java
+++ src/main/java/com/practicasesfe/Formularios/Horarios.java
@@ -4,6 +4,20 @@ import javax.swing.*;
 
 public class Horarios extends JPanel {
     private JPanel rootPanel;
+    private JTable table1;
+    private JTextField textField1;
+    private JSpinner spinner1;
+    private JSpinner spinner2;
+    private JTextField textField2;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+
     public JPanel getRootPanel() {
         return rootPanel;
     }
