7a0418daddb5d4d5ee4eac1855aa66f45a3315b7
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-06-15 13:18:46 -0600

    Se edito y termino el diseño del formulario de horarios

--- src/main/java/com/practicasesfe/Formularios/Horarios.java
+++ src/main/java/com/practicasesfe/Formularios/Horarios.java
@@ -9,14 +9,14 @@ public class Horarios extends JPanel {
     private JSpinner spinner1;
     private JSpinner spinner2;
     private JTextField textField2;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton button7;
-    private JButton button8;
+    private JButton guardarButton;
+    private JButton cancelarButton;
+    private JButton buscarButton;
+    private JButton nuevoButton;
+    private JButton editarButton;
+    private JButton verButton;
+    private JButton eliminarButton;
+    private JButton volverButton;
 
     public JPanel getRootPanel() {
         return rootPanel;
