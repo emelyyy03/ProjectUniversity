8ed8b89698210cfd61ebc5c82571e8d3085574bf
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-06-15 01:35:18 -0600

    Se creo el formulario de ciclos su diseño

--- src/main/java/com/practicasesfe/Formularios/Ciclos.java
+++ src/main/java/com/practicasesfe/Formularios/Ciclos.java
@@ -3,4 +3,17 @@ package com.practicasesfe.Formularios;
 import javax.swing.*;
 
 public class Ciclos extends JPanel {
+    private JPanel rootPanel;
+    private JTextField textField1;
+    private JSpinner spinner1;
+    private JSpinner spinner2;
+    private JTable table1;
+    private JButton volverButton;
+    private JButton guardarButton;
+    private JButton cancelarButton;
+    private JButton buscarButton;
+    private JButton crearButton;
+    private JButton editarButton;
+    private JButton verButton;
+    private JButton eliminarButton;
 }
